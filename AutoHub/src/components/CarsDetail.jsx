220651474fc58c6492d1a4fa599976a1b8eb736b
Walterhart <walterwhart1@gmail.com>
2023-07-07 14:37:36 -0400

    Add React icons and add css.

--- AutoHub/src/components/CarsDetail.jsx
+++ AutoHub/src/components/CarsDetail.jsx
@@ -1,13 +1,9 @@
-import React from "react"
-import { Link } from "react-router-dom"
-export default function Header() {
-    return (
-        <header>
-            <Link className="site-logo" to="/">AutoHub</Link>
-            <nav>
-                <Link to="/about">About</Link>
-                <Link to="/cars">Cars</Link>
-            </nav>
-        </header>
-    )
+import React from 'react'
+
+function CarsDetail() {
+  return (
+    <div>CarsDetail</div>
+  )
 }
+
+export default CarsDetail
--- AutoHub/src/components/Header.jsx
+++ AutoHub/src/components/Header.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
-
-function Header() {
-  return (
-    <div>Header</div>
-  )
+import React from "react"
+import { Link } from "react-router-dom"
+export default function Header() {
+    return (
+        <header>
+            <Link className="site-logo" to="/">AutoHub</Link>
+            <nav>
+                <Link to="/about">About</Link>
+                <Link to="/cars">Cars</Link>
+            </nav>
+        </header>
+    )
 }
-
-export default Header
