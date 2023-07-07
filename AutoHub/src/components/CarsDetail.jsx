f65b8e66e64e2f23bc1def455620383376c9ca59
Walterhart <walterwhart1@gmail.com>
2023-07-07 15:57:18 -0400

    Use route params to display  detail about selected id

--- AutoHub/src/components/CarsDetail.jsx
+++ AutoHub/src/components/CarsDetail.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react"
+import { useParams } from "react-router-dom"
 
-function CarsDetail() {
-  return (
-    <div>CarsDetail</div>
-  )
+export default function CarDetail() {
+    const params = useParams()
+    const [car, setCar] = React.useState(null)
+
+    React.useEffect(() => {
+        fetch(`/api/cars/${params.id}`)
+            .then(res => res.json())
+            .then(data => setCar(data.cars))
+    }, [params.id])
+
+    return (
+        <div className="car-detail-container">
+            {car ? (
+                <div className="car-detail">
+                    <img src={car.imageUrl} />
+                    <i className={`car-brand ${car.brand} selected`}>{car.brand}</i>
+                    <h2>{car.model}</h2>
+                    <p className="car-price"><span>${car.price}</span></p>
+                    <p>{car.description}</p>
+                    <button className="link-button">Buy this car</button>
+                </div>
+            ) : <h2>Loading...</h2>}
+        </div>
+    )
 }
-
-export default CarsDetail
