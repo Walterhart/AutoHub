dfd0ded8224c849463155268f84ffff0c04f73c6
Walterhart <walterwhart1@gmail.com>
2023-07-07 15:27:45 -0400

    Add functionality to display cars on cars page

--- AutoHub/src/pages/Cars.jsx
+++ AutoHub/src/pages/Cars.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react"
+import { Link } from "react-router-dom"
 
-function Cars() {
-  return (
-    <div>Cars</div>
-  )
+export default function Cars() {
+    const [cars, setCars] = React.useState([])
+    React.useEffect(() => {
+        fetch("/api/cars")
+            .then(res => res.json())
+            .then(data => setCars(data.cars))
+    }, [])
+    console.log(cars)
+
+    const carsElements = cars.map(cars => (
+        <div key={cars.id} className="car-tile">
+            <Link to={`/cars/${cars.id}`}>
+                <img src={cars.imageUrl} />
+                <div className="car-info">
+                    <h3>{cars.model}</h3>
+                    <p>${cars.price}</p>
+                </div>
+                <i className={`car-brand ${cars.brand} selected`}>{cars.brand}</i>
+            </Link>
+        </div>
+    ))
+
+    return (
+        <div className="car-list-container">
+            <h1>Explore our cars options</h1>
+            <div className="car-list">
+                {carsElements}
+            </div>
+        </div>
+    )
 }
-
-export default Cars
--- AutoHub/src/server.js
+++ AutoHub/src/server.js
@@ -2,12 +2,13 @@ import { createServer, Model } from "miragejs";
 
 createServer({
   models: {
-    car: Model,
+    cars: Model,
   },
 
   seeds(server) {
     server.create("car", {
         id: "1",
+        brand: "Toyota",
         price: 25000,
         description:
           "The Toyota Camry is a mid-size sedan known for its reliability and fuel efficiency. It offers a comfortable ride and is equipped with advanced safety features.",
@@ -16,8 +17,10 @@ createServer({
           "https://images.unsplash.com/photo-1621007947382-bb3c3994e3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2670&q=80",
         type: "sedan",
       });
-      server.create("car", {
+      
+    server.create("car", {
         id: "2",
+        brand: "Ford",
         price: 40000,
         description:
           "The Ford Mustang is a legendary sports car known for its powerful performance and iconic design. It delivers thrilling acceleration and offers a thrilling driving experience.",
@@ -27,8 +30,9 @@ createServer({
         type: "sports car",
       });
  
-      server.create("car", {
+    server.create("car", {
         id: "3",
+        brand: "Tesla",
         price: 50000,
         description:
           "The Tesla Model 3 is an all-electric sedan known for its cutting-edge technology and impressive range. It offers a smooth and quiet ride while being environmentally friendly.",
@@ -37,18 +41,18 @@ createServer({
           "https://images.unsplash.com/photo-1571127236794-81c0bbfe1ce3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3132&q=80",
         type: "electric car",
       });
-  },
+},
 
   routes() {
     this.namespace = "api";
 
     this.get("/cars", (schema, request) => {
-      return schema.car.all();
+      return schema.cars.all();
     });
 
     this.get("/cars/:id", (schema, request) => {
       const id = request.params.id;
-      return schema.car.find(id);
+      return schema.cars.find(id);
     });
   },
 });
