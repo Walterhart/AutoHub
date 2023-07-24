40d3b74472bc9f8a3f22cc56ce507a7ae82522a2
Walterhart <walterwhart1@gmail.com>
2023-07-24 13:48:31 -0400

    Add loading to CarDetail

--- AutoHub/src/pages/Cars/CarDetail.jsx
+++ AutoHub/src/pages/Cars/CarDetail.jsx
@@ -1,39 +1,47 @@
-import React, { useEffect } from "react";
-import { Link, useLocation, useLoaderData } from "react-router-dom";
+import React, { Suspense, useEffect } from "react";
+import { Link, useLocation, useLoaderData, Await, defer } from "react-router-dom";
 import { getCars } from "../../api";
+import LoadingSpinner from "../../components/LoadingSpinner";
 
 export function loader({ params }) {
-  return getCars(params.id);
+  return defer({car: getCars(params.id)}) ;
 }
 
 export default function CarDetail() {
   const location = useLocation();
-  const car = useLoaderData();
+  const dataPromise = useLoaderData();
 
   const search = location.state?.search || "";
   const brand = location.state?.brand || "all";
+  const renderCar = (car) =>{
+
+    return (
+      <div className="car-detail-container">
+          <div className="car-detail">
+            <Link to={`..${search}`} relative="path" className="back-button">
+              &larr;
+              <span>
+                Back to <span className="capitalize">{brand}</span> vehicles
+              </span>
+            </Link>
+            <img src={car.imageUrl} />
+            <i className={`car-brand ${car.brand} selected capitalize `}>{car.brand}</i>
+            <h2 className="capitalize">{car.model}</h2>
+            <p className="car-price">
+              <span>${car.price}</span>
+            </p>
+            <p>{car.description}</p>
+            <button className="link-button">Buy this car</button>
+          </div> 
+      </div>
+    );
+  }
+  
   return (
-    <div className="car-detail-container">
-      {car ? (
-        <div className="car-detail">
-          <Link to={`..${search}`} relative="path" className="back-button">
-            &larr;{" "}
-            <span>
-              Back to <span className="capitalize">{brand}</span> vehicles
-            </span>
-          </Link>
-          <img src={car.imageUrl} />
-          <i className={`car-brand ${car.brand} selected`}>{car.brand}</i>
-          <h2>{car.model}</h2>
-          <p className="car-price">
-            <span>${car.price}</span>
-          </p>
-          <p>{car.description}</p>
-          <button className="link-button">Buy this car</button>
-        </div>
-      ) : (
-        <h2>Loading...</h2>
-      )}
-    </div>
+    <Suspense fallback={<LoadingSpinner/>}>
+    <Await resolve={dataPromise.car}>
+      {renderCar}
+    </Await>
+    </Suspense>
   );
 }
