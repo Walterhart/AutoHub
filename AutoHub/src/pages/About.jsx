8a5b8e7ae5b63c7df3d40fcde445d2930691fe46
Walterhart <walterwhart1@gmail.com>
2024-02-10 14:45:35 -0500

    Refactor about-page css

--- AutoHub/src/pages/About.jsx
+++ AutoHub/src/pages/About.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 
 export default function About() {
   return (
-    <div className="about-page-container">
-      <img src={image} className="about-hero-image" />
+    <div className="about-page-container flex-container">
+      
       <div className="about-page-content">
         <h1>Discover the Ultimate Ride at Autohub </h1>
         <p>At our dealership, we are committed to providing you with the best selection of cars to make your driving experience exceptional. Our mission is to match you with the perfect car that fits your style and preferences, ensuring every journey is a memorable one.</p>
@@ -15,6 +15,7 @@ export default function About() {
         <h2>Your Perfect Car Awaits.</h2>
         <Link className="link-button" to="/cars">Explore our inventory</Link>
       </div>
+      <img src={image} className="about-hero-image" />
     </div>
   );
 }
